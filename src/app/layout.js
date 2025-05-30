caf038644609c05b97fa05ab2832a700c1a610b6
LouisWongHK <LouisWongHK@protonmail.com>
2025-05-30 03:59:08 +0000

    GoogleAnalytics

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,8 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
+import { GoogleAnalytics } from '@next/third-parties/google'
+
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
@@ -22,6 +24,7 @@ export default function RootLayout({ children }) {
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
         {children}
       </body>
+      <GoogleAnalytics gaId="G-1MS2F677X1" />
     </html>
   );
 }
--- src/app/page.js
+++ src/app/page.js
@@ -334,6 +334,7 @@ export default function Home() {
           href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap"
           rel="stylesheet"
         />
+        
       </Head>
 
       <Header />
